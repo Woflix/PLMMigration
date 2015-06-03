c8cfb42f6d8e56a5163daabd3d3204fa2a1bbf19
Michael L <Hi@synergi.cc>
2015-06-03 17:11:39 +0800

    Multiple Edits
    + Added temporary hardcoded URL variables
    + Modified SQL error message
    + Modified script at end of file

--- imports/dataMigration.php
+++ imports/dataMigration.php
@@ -18,27 +18,30 @@ $counter = 2;
 
 if ($result->num_rows > 0) {
 	while($row = $result->fetch_assoc()) {
-		echo "<tr><td id=\"programid:".$row["ID"]."\" tabindex=0><a href=\"enditemdash.php?programID=".$row["programid"]."\" target=\"_blank\">".$row["programid"]."</a></td><td id=\"programname:".$row["ID"]."\" contenteditable=\"true\" tabindex=".$counter.">".$row["programname"]."</td><td id=\"assignedto:".$row["ID"]."\" contenteditable=\"true\" tabindex=".($counter+1).">".$row["assignedto"]."</td><td id=\"start:".$row["ID"]."\" contenteditable=\"true\" tabindex=".($counter+2).">".$row["start"]."</td><td id=\"end:".$row["ID"]."\" contenteditable=\"true\" tabindex=".($counter+3).">".$row["end"]."</td></tr>";
+		echo "<tr><td id=\"programid:".$row["ID"]."\" tabindex=0><a href=\"enditemdash.php?programID=".$row["programid"]."&user=Michael Leng&perm=Admin\" target=\"_blank\">".$row["programid"]."</a></td><td id=\"programname:".$row["ID"]."\" contenteditable=\"true\" tabindex=".$counter.">".$row["programname"]."</td><td id=\"assignedto:".$row["ID"]."\" contenteditable=\"true\" tabindex=".($counter+1).">".$row["assignedto"]."</td><td id=\"start:".$row["ID"]."\" contenteditable=\"true\" tabindex=".($counter+2).">".$row["start"]."</td><td id=\"end:".$row["ID"]."\" contenteditable=\"true\" tabindex=".($counter+3).">".$row["end"]."</td></tr>";
 		$counter = $counter + 4;
 	}
 } else {
-	echo "No data could be retrieved.";
+	echo "<h2 id=\"sqlError\">Error: No data could be retrieved.</h2>";
 }
 $conn->close();
 ?>
 <script type="text/javascript">
 	$(function(){
-		$('td[contenteditable=true]').blur(function(){
-			var field_userid = $(this).attr("id");
-			var value = $(this).text();
-			$.post('imports/ajaxMigration.php', field_userid + "=" + value, function(data){
-				if(data != ''){
-					$('.statusPushed').addClass("statusPushedShow");
-					setTimeout (function(){
-						$('.statusPushed').removeClass("statusPushedShow");
-					}, 1000);
-				}
+			$('td[contenteditable=true]').blur(function(){
+				var field_userid = $(this).attr("id");
+				var value = $(this).text();
+				$.post('imports/ajaxMigration.php', field_userid + "=" + value, function(data){
+					if(data != ''){
+						$('.statusUpdated').removeClass("statusUpdatedShow");
+						setTimeout (function(){
+							$('.statusPushed').addClass("statusPushedShow");
+						}, 300);
+						setTimeout (function(){
+							$('.statusPushed').removeClass("statusPushedShow");
+						}, 1300);
+					}
+				});
 			});
 		});
-	});
 </script>
