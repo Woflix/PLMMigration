288598a3ce1909d238381fb7a5a9a7d9b45cce2e
Michael L <Hi@synergi.cc>
2015-06-03 17:10:19 +0800

    Added file 'authMain.php'
    + Added file ‘authMain.php’ in folder ‘imports’

---
+++ imports/authMain.php
@@ -0,0 +1,34 @@
+<?php
+	/*$field_name = $_POST;
+	$field_user = strip_tags(trim($field_name));
+	echo $field_user;
+	echo "test";*/
+	$username = $_POST["username"];
+	echo htmlspecialchars($username);
+	echo "test";
+?>
+
+<?php
+/*if(!empty($_POST))
+{
+	include "config.php";
+	foreach($_POST as $field_name => $val)
+	{
+		$field_userid = strip_tags(trim($field_name));
+		$val = strip_tags(trim(mysql_real_escape_string($val)));
+		
+		$split_data = explode(':', $field_userid);
+		$user_id = $split_data[1];
+		$field_name = $split_data[0];
+		if(!empty($user_id) && !empty($field_name) && !empty($val))
+		{
+			mysql_query("UPDATE programs SET $field_name = '$val' WHERE ID = $user_id") or mysql_error();
+			echo "Updated";
+		} else {
+			echo "Invalid Requests";
+		}
+	}
+} else {
+	echo "Invalid Requests";
+}*/
+?>
