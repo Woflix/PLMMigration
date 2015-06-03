1c37b45eb376b2d8ecd226a0350d2be0d226db7c
Michael L <Hi@synergi.cc>
2015-06-03 10:32:51 +0800

    Added file 'newprogramdash.php'
    + Added file ‘newprogramdash.php’

---
+++ newprogramdash.php
@@ -0,0 +1,69 @@
+<head>
+	<meta charset="utf-8">
+	<link rel="shortcut icon" type="image/EXT" href="">
+	<title>Add New Program</title>
+
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<style type="text/css">
+		/*
+
+			This is temporary // For ease of editing
+
+		*/
+
+		body {
+			text-align: center;
+		}
+
+		#buttonGo {
+			padding: 20px 20px 20px 20px;
+			border: solid #000000 2px;
+			border-radius: 20px;
+			text-decoration: none;
+			font-size: 28px;
+			color: #000000;
+			transition: color 0.3s, background 0.3s;
+			-moz-transition: color 0.3s, background 0.3s;
+			-webkit-transition: color 0.3s, background 0.3s;
+			-o-transition: color 0.3s, background 0.3s;
+		}
+
+		#buttonGo:hover {
+			background: #000000;
+			color: #FFFFFF;
+		}
+
+		h1 {
+			margin: 150px 0px 30px 0px;
+		}
+
+		h2 {
+			margin: 20px 0px 80px 0px;
+		}
+	</style>
+
+	<script type="text/javascript" language="javascript" src="js/jquery.js"></script>
+	<script type="text/javascript">
+		/* 
+
+			This is temporary // For ease of editing
+
+		*/
+
+
+
+		/*
+
+			This is permanent // For page load speed
+
+		*/
+
+		// Do stuff \\
+	</script>
+	<?php include 'imports/head.php'; ?>
+</head>
+<body>
+	<h1>Dash/New</h1>
+	<h2>Adding a new program has not yet been implemented.</h2>
+	<a href="migrationdash.php" id="buttonGo">Go To Migration Dashboard</a>
+</body>
