14ceb188d3aad99743e67b0d0e5ea91995eee971
Michael L <Hi@synergi.cc>
2015-06-03 13:53:08 +0800

    Multiple Edits
    + Added ‘ID’ to SQL query
    + Modified $counter value
    + Added tab indexing to echo
    + Added push script to end of file

--- imports/dataMigration.php
+++ imports/dataMigration.php
@@ -11,18 +11,34 @@ if ($conn->connect_error) {
 	echo "Connected.";
 }*/
 
-$sql = "SELECT programid, programname, assignedto, start, end FROM programs";
+$sql = "SELECT programid, programname, assignedto, start, end, ID FROM programs";
 $result = $conn->query($sql);
 
-$counter = 1;
+$counter = 2;
 
 if ($result->num_rows > 0) {
 	while($row = $result->fetch_assoc()) {
-		echo "<tr><td id=\"programid:".$counter."\"><a href=\"enditemdash.php?programID=".$row["programid"]."\" target=\"_blank\">".$row["programid"]."</a></td><td id=\"programname:".$counter."\" contenteditable=\"true\">".$row["programname"]."</td><td id=\"assignedto:".$counter."\" contenteditable=\"true\">".$row["assignedto"]."</td><td id=\"start:".$counter."\" contenteditable=\"true\">".$row["start"]."</td><td id=\"end:".$counter."\" contenteditable=\"true\">".$row["end"]."</td></tr>";
-		$counter = $counter + 1;
+		echo "<tr><td id=\"programid:".$row["ID"]."\" tabindex=0><a href=\"enditemdash.php?programID=".$row["programid"]."\" target=\"_blank\">".$row["programid"]."</a></td><td id=\"programname:".$row["ID"]."\" contenteditable=\"true\" tabindex=".$counter.">".$row["programname"]."</td><td id=\"assignedto:".$row["ID"]."\" contenteditable=\"true\" tabindex=".($counter+1).">".$row["assignedto"]."</td><td id=\"start:".$row["ID"]."\" contenteditable=\"true\" tabindex=".($counter+2).">".$row["start"]."</td><td id=\"end:".$row["ID"]."\" contenteditable=\"true\" tabindex=".($counter+3).">".$row["end"]."</td></tr>";
+		$counter = $counter + 4;
 	}
 } else {
 	echo "No data could be retrieved.";
 }
 $conn->close();
 ?>
+<script type="text/javascript">
+	$(function(){
+		$('td[contenteditable=true]').blur(function(){
+			var field_userid = $(this).attr("id");
+			var value = $(this).text();
+			$.post('imports/ajaxMigration.php', field_userid + "=" + value, function(data){
+				if(data != ''){
+					$('.statusPushed').addClass("statusPushedShow");
+					setTimeout (function(){
+						$('.statusPushed').removeClass("statusPushedShow");
+					}, 1000);
+				}
+			});
+		});
+	});
+</script>
