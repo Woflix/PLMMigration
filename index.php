81e45f14639aa3102c6c0e80786407727cbc29dd
Michael L <Hi@synergi.cc>
2015-06-03 17:14:49 +0800

    Multiple Edits
    + Added ‘normalize.css’
    + Added and modified some styles for #buttonGo
    + Added styles for #login, #usernameField, #passwordField,
    .statusError, and .loginFooter
    + Added temporary authentication function (currently does not work)
    + Added form and inputs
    + Added temporary button to ‘migrationdash.php’
    + Added footer and statusWorking wrapper
    + Added status messages

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 	<title>Dashboard Login</title>
 
 	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/normalize.css">
 	<style type="text/css">
 		/*
 
@@ -16,12 +17,15 @@
 		}
 
 		#buttonGo {
-			padding: 20px 20px 20px 20px;
-			border: solid #000000 2px;
-			border-radius: 20px;
+			display: block;
+			padding: 10px 10px 10px 10px;
+			border: solid #000000 1px;
+			border-radius: 15px;
 			text-decoration: none;
-			font-size: 28px;
+			font-size: 16px;
 			color: #000000;
+			width: 280px;
+			margin: 30px auto 0px auto;
 			transition: color 0.3s, background 0.3s;
 			-moz-transition: color 0.3s, background 0.3s;
 			-webkit-transition: color 0.3s, background 0.3s;
@@ -33,12 +37,35 @@
 			color: #FFFFFF;
 		}
 
-		h1 {
-			margin: 150px 0px 30px 0px;
+		#login {
+			text-align: center;
 		}
 
-		h2 {
-			margin: 20px 0px 80px 0px;
+		#usernameField {
+			display: block;
+			margin: 20px 0px 20px 0px;
+			width: 300px;
+			height: 40px;
+			border-radius: 15px;
+			margin: 0 auto;
+		}
+
+		#passwordField {
+			display: block;
+			margin: 20px 0px 20px 0px;
+			width: 300px;
+			height: 40px;
+			border-radius: 15px;
+			margin: 0 auto;
+		}
+
+		.statusError {
+			width: 70%;
+			margin: 0 auto;
+		}
+
+		.loginFooter {
+			margin-top: 80px;
 		}
 	</style>
 
@@ -58,12 +85,47 @@
 
 		*/
 
-		// Do stuff \\
+		// Authentication Function \\
+		function auth(){
+			var field_user = $('#usernameField').attr("id");
+			var value = $('#usernameField').val();
+			alert(value);
+			$.post('imports/authMain.php', "username=" + value, function(data){
+				if(data != ''){
+					$('.statusWorking').addClass("statusWorkingShow");
+					setTimeout (function(){
+						$('.statusWorking').removeClass("statusWorkingShow");
+					}, 1000);
+					$('.debug').load('imports/authMain.php');
+				}
+			});
+		}
 	</script>
 	<?php include 'imports/head.php'; ?>
 </head>
 <body>
-	<h1>Dash/Log</h1>
-	<h2>Login and authentication is not yet implemented.</h2>
-	<a href="migrationdash.php" id="buttonGo">Go To Migration Dashboard</a>
+	<div class="header">
+		<h1>Dash/Log</h1>
+		<h2>Login and authentication is not yet implemented.</h2>
+	</div>
+	<form id="login">
+		<p id="formTitle">Username</p>
+		<input type="text" id="usernameField"/>
+		<p id="formTitle">Password</p>
+		<input type="password" id="passwordField"/>
+		<a href="#" id="buttonGo" onclick="auth();">Login</a>
+		<a href="migrationdash.php?user=Michael Leng&amp;perm=Admin" id="buttonGo">Go to Migration Dashboard (DEBUG)</a>
+	</form>
+	<div class="footer loginFooter">
+		<div class="statusWrapper">
+			<div class="statusWorking">
+				<h4>Authenticating</h4>
+			</div>
+			<div class="statusError">
+				<h4>Unable to log in. Please ensure your username and password are correct.</h4>
+			</div>
+			<div class="debug">
+			</div>
+		</div>
+	</div>
 </body>
