b895f9b3b9a30e2b4d97e33616aa45047391fef1
Michael L <Hi@synergi.cc>
2015-06-03 17:09:46 +0800

    Cleaned up Syntax
    + Cleaned up some code

--- imports/ajaxEnditem.php
+++ imports/ajaxEnditem.php
@@ -1,17 +1,14 @@
 <?php
-if(!empty($_POST))
-{
+if(!empty($_POST)){
 	include "config.php";
-	foreach($_POST as $field_name => $val)
-	{
+	foreach($_POST as $field_name => $val){
 		$field_userid = strip_tags(trim($field_name));
 		$val = strip_tags(trim(mysql_real_escape_string($val)));
 		
 		$split_data = explode(':', $field_userid);
 		$user_id = $split_data[1];
 		$field_name = $split_data[0];
-		if(!empty($user_id) && !empty($field_name) && !empty($val))
-		{
+		if(!empty($user_id) && !empty($field_name) && !empty($val)){
 			mysql_query("UPDATE enditems SET $field_name = '$val' WHERE ID = $user_id") or mysql_error();
 			echo "Updated";
 		} else {
