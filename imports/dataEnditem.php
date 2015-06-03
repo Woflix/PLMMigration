b5fb75c74b3c44dc44710ec42af49ff076cac4ff
Michael L <Hi@synergi.cc>
2015-06-03 13:52:52 +0800

    Multiple Edits
    + Added ‘ID’ to SQL query
    + Modified $counter value
    + Added tab indexing to echo
    + Added push script to end of file

--- imports/dataEnditem.php
+++ imports/dataEnditem.php
@@ -13,18 +13,34 @@ if ($conn->connect_error) {
 
 $programID = $_GET["programID"];
 
-$sql = "SELECT originalname, renamedto, latestrev, datatype, jobsfolder FROM enditems WHERE programid=$programID";
+$sql = "SELECT originalname, renamedto, latestrev, datatype, jobsfolder, ID FROM enditems WHERE programid=$programID";
 $result = $conn->query($sql);
 
-$counter = 0;
+$counter = 2;
 
 if ($result->num_rows > 0) {
 	while($row = $result->fetch_assoc()) {
-		echo "<tr><td id=\"actions:".$counter."\"><a href=\"#\" onclick=\"alertC(\" \");\">A</a><a href=\"#\" onclick=\"alertC(\" \");\">B</a><a href=\"#\" onclick=\"alertC(\" \");\">C</a></td><td id=\"originalname:".$counter."\" contenteditable=\"true\">".$row["originalname"]."</td><td id=\"renamedto:".$counter."\" contenteditable=\"true\">".$row["renamedto"]."</td><td id=\"latestrev:".$counter."\" contenteditable=\"true\">".$row["latestrev"]."</td><td id=\"datatype:".$counter."\" contenteditable=\"true\">".$row["datatype"]."</td><td id=\"jobsfolder:".$counter."\"><a href=\"".$row["jobsfolder"]."\" title=\"Download from: ".$row["jobsfolder"]."\" target=\"_blank\">Download</a></td></tr>";
-		$counter = $counter + 1;
+		echo "<tr><td id=\"actions:".$row["ID"]."\" tabindex=0><a href=\"#\" onclick=\"alertC(\" \");\">A</a><a href=\"#\" onclick=\"alertC(\" \");\">B</a><a href=\"#\" onclick=\"alertC(\" \");\">C</a></td><td id=\"originalname:".$row["ID"]."\" contenteditable=\"true\" tabindex=".$counter.">".$row["originalname"]."</td><td id=\"renamedto:".$row["ID"]."\" contenteditable=\"true\" tabindex=".($counter+1).">".$row["renamedto"]."</td><td id=\"latestrev:".$row["ID"]."\" contenteditable=\"true\" tabindex=".($counter+2).">".$row["latestrev"]."</td><td id=\"datatype:".$row["ID"]."\" contenteditable=\"true\" tabindex=".($counter+3).">".$row["datatype"]."</td><td id=\"jobsfolder:".$row["ID"]."\" tabindex=0><a href=\"".$row["jobsfolder"]."\" title=\"Download from: ".$row["jobsfolder"]."\" target=\"_blank\">Download</a></td></tr>";
+		$counter = $counter + 4;
 	}
 } else {
 	echo "No data could be retrieved.";
 }
 $conn->close();
 ?>
+<script type="text/javascript">
+	$(function(){
+		$('td[contenteditable=true]').blur(function(){
+			var field_userid = $(this).attr("id");
+			var value = $(this).text();
+			$.post('imports/ajaxMigration.php', field_userid + "=" + value, function(data){
+				if(data != ''){
+					$('.statusPushed').addClass("statusPushedShow");
+					setTimeout (function(){
+						$('.statusPushed').removeClass("statusPushedShow");
+					}, 1000);
+				}
+			});
+		});
+	});
+</script>
