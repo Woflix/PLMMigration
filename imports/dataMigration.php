bc9a6bdf63596b3e80603d636e1dc7923535ded2
Michael L <Hi@synergi.cc>
2015-06-05 01:36:45 +0800

    Added 'configData.php'
    + Removed hardcoded SQL server variables
    + Added include for ‘configData.php’

--- imports/dataMigration.php
+++ imports/dataMigration.php
@@ -1,8 +1,5 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "migration";
+include 'configData.php';
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
