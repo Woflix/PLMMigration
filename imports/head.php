b1badbf77663119b9c4073c41828fe6d40f6be04
Michael L <Hi@synergi.cc>
2015-06-05 17:26:07 +0800

    Multiple Edits
    + Added functions

--- imports/head.php
+++ imports/head.php
@@ -1,3 +1,19 @@
-<!-- START HEAD.PHP -->
+		<!-- START HEAD.PHP -->
+		<script type="text/javascript">
+			// Opens Help Overlay \\
+			function openHelpOverlay(){
+				$('.helpOverlay').addClass('helpOverlayShowDisplay');
+				setTimeout (function(){
+					$('.helpOverlay').addClass('helpOverlayShow');
+				}, 100);	
+			}
 
-<!-- END HEAD.PHP -->
+			// Closes Help Overlay \\
+			function closeHelpOverlay(){
+				$('.helpOverlay').removeClass('helpOverlayShow');
+				setTimeout (function(){
+					$('.helpOverlay').removeClass('helpOverlayShowDisplay');
+				}, 400);
+			}
+		</script>
+		<!-- END HEAD.PHP -->
