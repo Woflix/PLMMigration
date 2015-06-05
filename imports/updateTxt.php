ae9b4608cd6171ebe3f896efa2d5fe00f8e4307f
Michael L <Hi@synergi.cc>
2015-06-05 17:30:20 +0800

    Added Comments
    + Added comments

--- imports/updateTxt.php
+++ imports/updateTxt.php
@@ -1,5 +1,8 @@
 <?php 
+// Get URL Variable \\
 $data = $_GET["info"];
+// Append Contents of URL Variable with New Line to 'joblist.txt' \\
 $txtFile = file_put_contents("joblist.txt", $data.PHP_EOL, FILE_APPEND);
+// Show Status Message \\
 echo "<h2 id=\"sqlGood\">Successfully updated joblist.txt.</h2>";
 ?>
