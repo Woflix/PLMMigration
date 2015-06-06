15abdfa94e95bfd152c0f540890ee4e5b74a3840
Michael L <Hi@synergi.cc>
2015-06-06 17:39:01 +0800

    Modified Status Message
    + Modified status message

--- imports/updateTxt.php
+++ imports/updateTxt.php
@@ -3,6 +3,5 @@
 $data = $_GET["info"];
 // Append Contents of URL Variable with New Line to 'joblist.txt' \\
 $txtFile = file_put_contents("joblist.txt", $data.PHP_EOL, FILE_APPEND);
-// Show Status Message \\
-echo "<h2 id=\"sqlGood\">Successfully updated joblist.txt.</h2>";
 ?>
+<script type="text/javascript">showGoodSuccess();</script>
